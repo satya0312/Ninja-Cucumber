95d7e085cb35c9b405de1aa5f01eca21215fd5dd
satya <tsatya0312@gmail.com>
2023-04-03 18:36:40 +0530

    register page step defination

--- src/test/java/com/tnp/ninja/qa/stepdefination/Register.java
+++ src/test/java/com/tnp/ninja/qa/stepdefination/Register.java
@@ -1,14 +1,14 @@
 package com.tnp.ninja.qa.stepdefination;
 
-import java.time.Duration;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.tnp.ninja.qa.factory.DriverFactory;
 
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -16,22 +16,11 @@ import io.cucumber.java.en.When;
 public class Register {
 	WebDriver driver;
 
-	@Before
-	public void setup() {
-		driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("http://www.tutorialsninja.com/demo/");
-	}
-
-	@After
-	public void teardown() {
-		driver.quit();
-	}
 
 	@Given("User Navigate to Register page")
 	public void user_navigate_to_register_page() {
+		
+		driver= DriverFactory.getDriver();
 		driver.findElement(By.linkText("My Account")).click();
 		driver.findElement(By.linkText("Register")).click();
 
@@ -40,8 +29,14 @@ public class Register {
 	@When("User provide the below fields")
 	public void user_provide_the_below_fields(DataTable dataTable) {
 		
-		
-	}
+		Map<String, String> dataMap = dataTable.asMap(String.class,String.class);
+		driver.findElement(By.name("firstname")).sendKeys(dataMap.get("FirstName"));
+		driver.findElement(By.name("lastname")).sendKeys(dataMap.get("LastName"));
+		driver.findElement(By.name("email")).sendKeys(dataMap.get("E-Mail"));
+		driver.findElement(By.name("telephone")).sendKeys(dataMap.get("Telephone"));
+		driver.findElement(By.name("password")).sendKeys(dataMap.get("Password"));
+		driver.findElement(By.id("input-confirm")).sendKeys(dataMap.get("Password"));
+	} 
 
 	@When("User select the agree Checkbox")
 	public void user_select_the_agree_checkbox() {
@@ -57,7 +52,8 @@ public class Register {
 
 	@Then("User Account should be created sucessfully")
 	public void user_account_should_be_created_sucessfully() {
-		;
+		String ActualSucessMessage = driver.findElement(By.xpath("//*[@id=\"content\"]/h1")).getText();
+	Assert.assertTrue(ActualSucessMessage.contains("Your Account Has Been Created!"));
 	}
 
 	@When("User select the NewsLetter as yes radio button")
@@ -68,6 +64,8 @@ public class Register {
 
 	@Then("User should get a proper warning message about duplicate account")
 	public void user_should_get_a_proper_warning_message_about_duplicate_account() {
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText().contains("Warning: E-Mail Address is already registered!"));
 
 	}
 
@@ -78,6 +76,11 @@ public class Register {
 
 	@Then("User should get a proper warning message about Manditatory fields")
 	public void user_should_get_a_proper_warning_message_about_manditatory_fields() {
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id=\"input-firstname\"]/following-sibling::div")).getText().contains("First Name must be between 1 and 32 characters!"), "FirstName warningmessgae is not displayed");
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id=\"input-lastname\"]/following-sibling::div")).getText().contains("Last Name must be between 1 and 32 characters!"),"LastName warningmessgae is not displayed");
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id=\"input-email\"]/following-sibling::div")).getText().contains("Last Name must be between 1 and 32 characters!"),"E-Mail Address does not appear to be valid!");
 
 	}
 
--- src/test/resourses/com/tnp/ninja/qa/features/Register.feature
+++ src/test/resourses/com/tnp/ninja/qa/features/Register.feature
@@ -3,12 +3,12 @@ Feature: Register Functionality
 Scenario: Register with only manditory Fields
 Given User Navigate to Register page
 When User provide the below fields
-|First Name				|Satya						|
-|Last Name				|Sa								|
+|FirstName				|Satya						|
+|LastName 				|Sa								|
 |E-Mail						|satya1@gmail.com	|
 |Telephone				|12345678 				|
 |Password					|123456						|
-|Password Confirm	|123456						|
+
 And User select the agree Checkbox
 And user click on submit button
 Then User Account should be created sucessfully
@@ -16,12 +16,12 @@ Then User Account should be created sucessfully
 Scenario: Register with All the Fields
 Given User Navigate to Register page
 When User provide the below fields
-|First Name				|Satya						|
-|Last Name				|Sa								|
+|FirstName				|Satya						|
+|LastName	  			|Sa								|
 |E-Mail						|satya1@gmail.com	|
 |Telephone				|12345678 				|
 |Password					|123456						|
-|Password Confirm	|123456						|
+
 And User select the NewsLetter as yes radio button
 And User select the agree Checkbox
 And user click on submit button
